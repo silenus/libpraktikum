291fe36b724fb04f0337618e8195d699c2f5d545
Martin Silenus <martin.silenus@gmx.de>
2010-03-05 13:04:24 +0100

    Added options.h, which defines a few options for root.
    I forgot to add add this and broke compilation of the repo. Sorry for
    that.

---
+++ options.h
@@ -0,0 +1,10 @@
+/** \file options.h
+ * This file defines a few options for the libpraktikum, mainly options concerning the appearance of root plots.
+ */
+
+/// The markerstyle used in plots
+#define MARKERSTYLE 21
+/// The size of the marker used in plots
+#define MARKERSIZE 0.5
+/// The standard count of bins for histogramms
+#define BINS 100
