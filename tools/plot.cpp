75bda8d7767ecfed307a39fe49904d7d69c3982e
Martin Silenus <martin.silenus@gmx.de>
2010-03-05 01:16:15 +0100

    Added command line options and multiple parameters to plot.
    Plot uses now getopt() to parse for command line option. You can also
    give an arbitrary number of cassy files as parameter and plot will
    process and plot them all.

--- tools/plot.cpp
+++ tools/plot.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
+#include <unistd.h>
 #include <TCanvas.h>
 #include <TGraph.h>
 #include <TAxis.h>
@@ -10,45 +11,69 @@
 
 using namespace std;
 int main(int argc, char* argv[]) {
-	if (argc < 2) {
-		cout << "Usage: plot path [column1 column2]" << endl;
-		return 0;
-	}
-	string filename(argv[1]);
-	Data data(filename);
+	// Parse for options
+	unsigned int xCol = 1;
+	unsigned int yCol = 2;
+	int c;
 
-	int col1 = 1;
-	int col2 = 2;
-	if (argc > 2) {
-		col1 = atoi(argv[2]);
-		col2 = atoi(argv[3]);
-	}
-	if (col1 < 1 || col2 < 1) {
+	opterr = 0;
+
+	while ((c = getopt (argc, argv, "hx:y:")) != -1)
+		switch (c) {
+			case 'h':
+				puts("Usage: plot [-x <column> -y <column>] FILES");
+				return 0;
+			case 'x':
+				xCol = atoi(optarg);
+				break;
+			case 'y':
+				yCol = atoi(optarg);
+				break;
+			case '?':
+				if (optopt == 'c')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr,
+							"Unknown option character `\\x%x'.\n",
+							optopt);
+				return 1;
+			default:
+				abort();
+		}
+	if (xCol < 1 || yCol < 1) {
 		cerr << "Error: Columns must be greater than 1" << endl;
 		return 1;
 	}
 
-	double *x = data.getCol(col1);
-	double *y = data.getCol(col2);
+	unsigned int length = argc - optind;
+	TApplication app(argv[0], NULL, NULL);
+	Data **data = new Data*[length];
 
-	TApplication app(argv[0], &argc, argv);
-	TCanvas *c1 = new TCanvas();
-	c1->SetGrid();
+	for (unsigned int i = 0; i < length; i++) {
+		data[i] = new Data(argv[optind + i]);
+		double *x = data[i]->getCol(xCol);
+		double *y = data[i]->getCol(yCol);
 
-	TGraph *graph = new TGraph(data.length(), x, y);
+		TCanvas *c1 = new TCanvas();
+		c1->SetGrid();
 
-	string title = "";
-	if (data.hasHeader()) {
-		title = data.getName(col1) + " [" + data.getUnit(col1) + "]";
-		graph->GetXaxis()->SetTitle(title.c_str());
-		title = data.getName(col2) + " [" + data.getUnit(col2) + "]";
-		graph->GetYaxis()->SetTitle(title.c_str());
+		TGraph *graph = new TGraph(data[i]->length(), x, y);
+
+		string title = "";
+		if (data[i]->hasHeader()) {
+			title = data[i]->getName(xCol) + " [" + data[i]->getUnit(xCol) + "]";
+			graph->GetXaxis()->SetTitle(title.c_str());
+			title = data[i]->getName(yCol) + " [" + data[i]->getUnit(yCol) + "]";
+			graph->GetYaxis()->SetTitle(title.c_str());
+		}
+		graph->SetMarkerStyle(21);
+		graph->SetMarkerSize(0.5);
+		if (title != "")
+			graph->SetTitle(title.c_str());
+		graph->Draw("APL");
 	}
-	graph->SetMarkerStyle(21);
-	graph->SetMarkerSize(0.5);
-	if (title != "")
-		graph->SetTitle(title.c_str());
-	graph->Draw("APL");
 
 	app.Run();
 	return 0;
